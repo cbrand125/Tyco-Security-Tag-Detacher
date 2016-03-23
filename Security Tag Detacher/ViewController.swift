d87ade6799dc1037376b61317e92b13d61e40580
Guest User <Guest@jackies-mbp-2.bad>
2016-03-23 00:07:10 -0400

    Initial Commit

---
+++ Security Tag Detacher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Security Tag Detacher
+//
+//  Created by Guest User on 3/23/16.
+//  Copyright © 2016 M E 440W Tyco Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
